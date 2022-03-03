b79749c14c9b887386f20784f4776dc451e9bdf4
Rasmus Luha <rasmus.luha@gmail.com>
2022-03-03 22:41:39 +0200

    Fixed info containter osa2

--- src/Components/SecondPageStuff/aboutMe.js
+++ src/Components/SecondPageStuff/aboutMe.js
@@ -4,9 +4,8 @@ const data = {
         "My name is Rasmus, the guy in the picture above.",
         "I am curreny studying computer scienece in University of Tartu, and so far I like it very much.",
         "I played football for 11 years, but now I go jogging every so often and bike alot due to the lack of drivers licence.",
-        "I can solve a Rubik's cube, quite fast.",
-        "Sometimes I read.",
-        "Watching Mr.robot was one of the big reasons why I started learning programming and also got into Linux.",
+        "I can solve a Rubik's cube, quite fast. Sometimes I read",
+        "Watching Mr. Robot was one of the big reasons why I started learning programming and also got into Linux.",
         "I am currenly drinking too much caffeine, but im turning things around.",
     ]
 }
